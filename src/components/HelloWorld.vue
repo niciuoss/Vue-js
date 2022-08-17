9b1d6b671868c697bea9e329a21b8ac3f509a591
Vinicius <viniciusamorimsales@gmail.com>
2022-08-17 00:52:47 -0300

    Projeto inicial em Vue js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,93 @@
 <template>
+<div>
+  <header>
+    <!-- Navbar -->
+    <MDBNavbar expand="lg" light bg="white" container>
+      <MDBNavbarToggler target="#navbarExample01"></MDBNavbarToggler>
+      <MDBNavbarNav collapse="navbarExample01" class="mb-2 mb-lg-0">
+        <MDBNavbarItem to="#" active>
+          Home
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          Features
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          Pricing
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          About
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBNavbar>
+    <!-- Navbar -->
+    <!-- Jumbotron -->
+    <div class="p-5 text-center bg-light">
+      <h1 class="mb-3">Store Items</h1>
+      <h4 class="mb-3">Subheading</h4>
+      <a class="btn btn-primary" href="" role="button"
+        >Call to action</a
+      >
+    </div>
+    <!-- Jumbotron -->
+  </header>
+<div v-if="storeitems.length">
+  <MDBRow :cols="['1','md-4']" class="g-4">
+  <div v-bind:key="storeitem.index" v-for="storeitem in storeitems">
+    <MDBCol>
+      <MDBCard>
+        <MDBCardImg :src="~/Documentos/Strapi/imgs" top alt="..."/>
+        <MDBCardBody>
+          <MDBCardTitle>{{storeitem.title}}</MDBCardTitle>
+          <MDBCardText>
+            {{storeitem.description}}
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  </div>
+  </MDBRow>
+  </div>
+</div>
+</template>
+<script>
+import { MDBNavbar, MDBNavbarToggler, MDBNavbarNav, MDBNavbarItem, MDBCol, MDBRow, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImg } from 'mdb-vue-ui-kit';
+export default {
+  name: 'HelloWorld',
+  components: {
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCol,
+    MDBRow,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImg
+  },
+  data(){
+    return {
+      storeitems: [],
+      imageLink: "http://localhost:1337/storeitems/"
+    }
+  },
+    mounted() {
+    fetch("http://localhost:1337/storeitems")
+      .then((res) => res.json())
+      .then((data) => {
+        this.storeitems = data;
+      });
+  }
+}
+</script>
+
+
+
+
+
+
+<!-- <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
@@ -37,10 +126,10 @@ export default {
     msg: String
   }
 }
-</script>
+</script> -->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!-- <style scoped>
 h3 {
   margin: 40px 0 0;
 }
@@ -55,4 +144,4 @@ li {
 a {
   color: #42b983;
 }
-</style>
+</style> -->
